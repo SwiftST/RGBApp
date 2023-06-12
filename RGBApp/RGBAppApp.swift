563f8ebb505d6139e4e51136733226b9146adf16
SwiftST <pavel.zubrilov@icloud.com>
2023-06-13 01:01:50 +0500

    Initial Commit

---
+++ RGBApp/RGBAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBAppApp.swift
+//  RGBApp
+//
+//  Created by Павел Зубрилов on 13.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct RGBAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
